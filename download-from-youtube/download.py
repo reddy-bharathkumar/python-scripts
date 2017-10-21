47c1a65b31a174c3f0a6a9745c75e6cf7aa76ceb
Bharath Kumar Reddy <reddy.bharathkumar@gmail.com>
2017-10-21 23:08:35 +0530

    Updated script(tested)
    Downloading both audio and video files from youtube URL. remove 36-40 lines if you want only Audio. Remove 42-45 if you want only Video format.

--- download-from-youtube/download.py
+++ download-from-youtube/download.py
@@ -1,33 +1,50 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 21 21:40:47 2017
+
+@author: bharath
+"""
 import pafy
 
-url="" #Enter the youtube URL
-#Initializing the video object using pafy.new() method
-video=pafy.new(url)
+url="https://www.youtube.com/watch?v=k9DMXBFHEH4" #Enter the youtube URL
 
-#Retrieving video properties
-title=video.title
-duration=video.duration
-date_published=video.published
-uploader=video.username
-best_getVideo=video.getbestvideo
-max_resolution_video=best_getVideo.resolution
-file_format_video=best_getVideo.extension
-file_size_video=best_getVideo.filesize()
-best_getAudio=video.getbestaudio
-file_format_audio=best_getAudio.extension
-file_size_audio=best_getAudio.filesize()
-
-print("Name: "+title)
-print("duration: "+duration)
-print("Uploaded On: "+date_published)
-print("Uploaded by: "+uploader)
-
-#Downloading the video with the best quality
-print("Downloading "+title+" with "+max_resolution_video+" resolution in "+file_format_video+" format")
-print("Size: "+file_size_video)
-best_getVideo.download(filepath="/Movies/"+title+file_format_video,quiet=False,callback=None,meta=False,remux_audio=False)
-
-#Download the audio with best quality
-print("Downloading audio "+title+" in "+file_format_audio+" format"
-print("Size: "+file_size_audio)
-best_getAudio.download(filepath="/Audio"+title+file_format_audio,quiet=False,callback=None,meta=False,remux_audio=False)
+def downloadVideo(url):
+    
+    #Initializing the video object using pafy.new() method
+    video=pafy.new(url)
+    
+    #Retrieving video properties
+    title=video.title
+    duration=video.duration
+    date_published=video.published
+    uploader=video.username
+    best_getVideo=video.getbest()
+    max_resolution_video=best_getVideo.resolution
+    file_format_video=best_getVideo.extension
+    file_size_video=best_getVideo.get_filesize()/1024/1024
+    best_getAudio=video.getbestaudio()
+    file_format_audio=best_getAudio.extension
+    file_size_audio=best_getAudio.get_filesize()/1024/1024
+    
+    print("Name: "+title)
+    print("duration: "+duration)
+    print("Uploaded On: "+date_published)
+    print("Uploaded by: "+uploader)
+    print("Resolution: "+max_resolution_video)
+    
+    #Downloading the video with the best quality
+    print("Downloading video"+title+" with "+max_resolution_video+" resolution in "+file_format_video+" format")
+    print("Format: "+file_format_video)
+    print("Size: "+str(file_size_video)+"MB")
+    best_getVideo.download(filepath=title+".mp4",quiet=False,callback=None,meta=False,remux_audio=False)
+    
+    #Download the audio with best quality
+    print("Downloading audio "+title+" in "+file_format_audio+" format"+best_getAudio.bitrate)
+    print("Size: "+str(file_size_audio)+"MB")
+    best_getAudio.download(filepath=title+"."+file_format_audio,quiet=False,callback=None,meta=False,remux_audio=False)
+    
+#try:    
+downloadVideo(url)
+#except:
+#    print("Sorry, this video can't be downloaded")
